8f67102adda8607fb372850d8669b976176c9dff
gerardo espinosa <ivanmtze96@gmail.com>
2021-03-27 11:17:08 -0600

    Home and drawer

---
+++ lib/landing/drawer.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:rowdy_hacks/login/AuthService.dart';
+
+class HomeDrawer extends StatefulWidget {
+  HomeDrawer({Key key}) : super(key: key);
+
+  @override
+  _HomeDrawerState createState() => _HomeDrawerState();
+}
+
+class _HomeDrawerState extends State<HomeDrawer> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text(
+              'My Profile',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text('Logout'),
+            onTap: () => {FirebaseAuth.instance.signOut()},
+          ),
+        ],
+      ),
+    );
+  }
+
+  logout(){
+    FirebaseAuth.instance.signOut();
+  }
+}
--- lib/landing/home.dart
+++ lib/landing/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rowdy_hacks/landing/drawer.dart';
 
 class Home extends StatefulWidget {
   Home({Key key}) : super(key: key);
@@ -8,10 +9,36 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+  int _selectedIndex = 0;
+  List<Widget> _widgetOptions = <Widget>[
+
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      drawer: HomeDrawer(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_city),
+            label: 'Places',
+            backgroundColor: Colors.pink,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
