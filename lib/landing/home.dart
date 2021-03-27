dd9cca1f084fe6ce8dc4022c8cae1a63c0f568e3
gerardo espinosa <ivanmtze96@gmail.com>
2021-03-27 10:11:34 -0600

    landing

--- lib/landing/home.dart
+++ lib/landing/home.dart
@@ -10,8 +10,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Hola"),
+    return Scaffold(
+      
     );
   }
 }
